bd4e67395f93d9a8e87f77b616a234e0ff706f3a
juanferrer437 <juanferrer437@uoc.edu>
2023-05-29 19:51:30 +0200

    caravaca

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
     theme: {
-    extend: {},
+     
+    extend: {
+      gridTemplateRows: {
+         'layout': '8vh 25vh 35vh',
+         
+      },
+      gridTemplateColumns : {
+        'layout':'1fr 3fr 1fr',
+        'header':'3fr 7fr',
+        'footer':'1fr 4fr 1fr'
+      }
+      
+    },
   },
   variants:{},
   plugins: [],
